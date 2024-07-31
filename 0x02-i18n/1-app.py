d822f79e43df87a777862b36c051f6a8a3189620
abeni-al7 <abenezeralebachew3@gmail.com>
2024-08-01 02:29:48 +0300

    Setup babel fix

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,27 +1,27 @@
 #!/usr/bin/env python3
-"""A simple flask application"""
-from flask import Flask, render_template
+"""A simple babel setup"""
 from flask_babel import Babel
+from flask import Flask, render_template
 
 
 class Config:
-    """A configuration class for babel"""
+    """A class for configuring flask babel"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
-app.config.from_object = Config
+app.config.from_object(Config)
 app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
-@app.route("/")
-def index() -> str:
-    """A simple route"""
-    return render_template("1-index.html")
+@app.route('/')
+def get_index() -> str:
+    """Returns a simple route"""
+    return render_template('1-index.html')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
