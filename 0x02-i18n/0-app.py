994d0dbfe4ff48cbb9d1791e6b17e8c6da4e1b5d
abeni-al7 <abenezeralebachew3@gmail.com>
2024-08-01 01:16:37 +0300

    Create a simple flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     """A simple route"""
-    return render_template("index.html")
+    return render_template("index.html") or "Hello world"
 
 
 if __name__ == "__name__":
