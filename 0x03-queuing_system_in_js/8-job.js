7929db85a90d41c604cc766606c1859852f02603
abeni-al7 <abenezeralebachew3@gmail.com>
2024-08-28 17:31:36 +0300

    Modularize job creation

---
+++ 0x03-queuing_system_in_js/8-job.js
@@ -0,0 +1,25 @@
+export default function createPushNotificationsJob(jobs, queue) {
+  if (!Array.isArray(jobs)) {
+    throw Error('Jobs is not an array');
+  }
+  jobs.forEach(obj => {
+    const job = queue.create('push_notification_code_3', obj)
+    .save((err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(`Notification job created: ${job.id}`);
+      }
+    });
+
+    job.on('complete', () => {
+      console.log(`Notification job ${job.id} completed`);
+    })
+    .on('failed', (err) => {
+      console.log(`Notification job ${job.id} failed: ${err}`);
+    })
+    .on('progress', (progress, data) => {
+      console.log(`Notification job ${job.id} ${progress}% complete`);
+    });
+  });
+}
